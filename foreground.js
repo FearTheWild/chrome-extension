e78fe66a0873f5cbcca91957fc66f5276af49b3c
Art Williams <art@fearthewild.com>
2021-03-11 17:52:33 +0000

    styling popup v1

--- foreground.js
+++ foreground.js
@@ -1,1 +1,9 @@
 console.log('From foreground');
+
+// Popup
+const popup = document.createElement('div');
+
+popup.id = 'ftw-popup';
+popup.innerHTML = '<h2>Posture check!</h2><p>Roll your shoulders back and push your head upright. Stretch your neck for 15 seconds left, right, and back.</p><a class="ftw-button">Finished</a>';
+
+document.body.appendChild(popup)
